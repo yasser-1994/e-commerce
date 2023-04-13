315cf1579389055ddaa6993093669f54cb4e2b9e
Yasser Mohammed <75188988+yasser-1994@users.noreply.github.com>
2023-04-13 21:32:16 +0200

    inti comiit

---
+++ js/main.js
@@ -0,0 +1,49 @@
+function accordion() {
+    let x = document.getElementById("click");
+    let y = document.getElementById("sec");
+    x.addEventListener("click", () => {
+      y.classList.toggle("active");
+      x.classList.toggle("change-color");
+    });
+  }
+  accordion();
+  
+  function accordion1() {
+    let x = document.getElementById("click2");
+    let y = document.getElementById("sec2");
+    x.addEventListener("click", () => {
+      y.classList.toggle("active");
+      x.classList.toggle("change-color");
+    });
+  }
+  accordion1();
+  
+  function accordion2() {
+    let x = document.getElementById("click3");
+    let y = document.getElementById("sec3");
+    x.addEventListener("click", () => {
+      y.classList.toggle("active");
+      x.classList.toggle("change-color");
+    });
+  }
+  accordion2();
+
+  function accordion3() {
+    let x = document.getElementById("click4");
+    let y = document.getElementById("sec4");
+    x.addEventListener("click", () => {
+      y.classList.toggle("active");
+      x.classList.toggle("change-color");
+    });
+  }
+  accordion3();
+// 
+  let click = document.getElementById('click-menu').onclick = function (){
+     let mydiv = document.getElementById('bar');
+     mydiv.classList.toggle('tarnslate');
+  } 
+  let close = document.getElementById('close').onclick = function (){
+    let mydiv = document.getElementById('bar');
+    mydiv.classList.toggle('tarnslate'); 
+  } 
+// 
